24ffd942cb1a35aa6e3ad8b87db9a337d61b3965
nlangellier <nlangellier@gmail.com>
2022-12-31 13:31:49 -0500

    add function docstrings for `compare_sort_outputs.py`

--- tests/compare_sort_outputs.py
+++ tests/compare_sort_outputs.py
@@ -8,6 +8,25 @@ ParsedSummaryFile = dict[str, File]
 
 
 def parse_summary_file(raw_summary_file: File) -> ParsedSummaryFile:
+    """
+    Parses a raw summary text file from ``Begum sort``.
+
+    Parameters
+    ----------
+    raw_summary_file
+        A list of strings where each string is one line of the summary file.
+
+    Returns
+    -------
+    ParsedSummaryFile
+        A dictionary where the keys are the section names of the summary file and the values are lists of strings where
+        each string is one line of the associated section in the summary file.
+
+    Raises
+    ------
+    AssertionError
+        If each section is not separated by a line of hyphens.
+    """
 
     section_separator = re.compile(pattern="^-+$")
 
@@ -35,6 +54,20 @@ def parse_summary_file(raw_summary_file: File) -> ParsedSummaryFile:
 
 
 def compare_summary_files(summary_files: tuple[ParsedSummaryFile, ...]) -> None:
+    """
+    Compares multiple (at least 2) summary files to ensure the contents are the same for each section of the summary
+    file.
+
+    Parameters
+    ----------
+    summary_files
+        A list of summary files where each file has the format returned by ``parse_summary_file``.
+
+    Raises
+    ------
+    AssertionError
+        If any 2 summary files differ in their contents.
+    """
 
     print("\nComparing summary files...")
 
@@ -51,6 +84,19 @@ def compare_summary_files(summary_files: tuple[ParsedSummaryFile, ...]) -> None:
 
 
 def compare_tag_info_files(tag_info_files: list[File]) -> None:
+    """
+    Compares multiple (at least 2) tag info files to ensure the contents are the same.
+
+    Parameters
+    ----------
+    tag_info_files
+        A list of tag info files where each file is a list of strings representing the lines of the associated file.
+
+    Raises
+    ------
+    AssertionError
+        If any 2 tag info files differ in their contents.
+    """
 
     print("\nComparing tag info files...")
 
@@ -64,6 +110,26 @@ def compare_tag_info_files(tag_info_files: list[File]) -> None:
 
 
 def text_file(file_path: str) -> File:
+    """
+    Returns the contents of a text file as a list of strings with each string representing each line of the file. This
+    function is intended to be used as the value of the ``type`` argument in ``argparse.ArgumentParser.add_argument``.
+
+    Parameters
+    ----------
+    file_path
+        The local file path of the file to open.
+
+    Returns
+    -------
+    File
+        The file contents as a list of strings representing the lines of the file.
+
+    Raises
+    ------
+    TypeError
+        All exceptions as caught and re-thrown as a ``TypeError`` so that ``argparse.ArgumentParser.parse_args`` will
+        return a useful error message (see <https://docs.python.org/3/library/argparse.html#type>).
+    """
 
     try:
         return Path(file_path).read_text().splitlines()
@@ -72,6 +138,20 @@ def text_file(file_path: str) -> File:
 
 
 def parse_cli_arguments() -> argparse.Namespace:
+    """
+    Parses the command line arguments.
+
+    Returns
+    -------
+    Namespace
+        The values of the parse command line arguments.
+
+    Raises
+    ------
+    AssertionError
+        If the number of summary files is not equal to the number of tag info files or if the number of files for either
+        is less than 2.
+    """
 
     parser = argparse.ArgumentParser()
 
@@ -87,6 +167,9 @@ def parse_cli_arguments() -> argparse.Namespace:
 
 
 def main() -> None:
+    """
+    Executes the comparison of outputs from multiple (at least 2) runs of ``Begum sort``.
+    """
 
     cli_args = parse_cli_arguments()
 
